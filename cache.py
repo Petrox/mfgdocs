b4b42fa57df40db53129fe9ba0eeb7cf9e988205
Peter Kopias <peter.kopias@rainboat.io>
2023-11-09 21:57:02 +0100

    add storage and cache functionality

---
+++ cache.py
@@ -0,0 +1,51 @@
+"""
+In-memory caching of key-value pairs with extra keys.
+
+"""
+class Cache:
+    """
+    Implements a flexible generic storage mechanism with multiple indexes to access data.
+
+    """
+    def __init__(self, name):
+        self.name = name
+        self.data = {}
+        self.index = {}
+
+    def clear(self):
+        self.index = {}
+        self.data = {}
+
+    def add(self, pk, value, extrakeys=None):
+        try:
+            self.data[pk] = value
+            if extrakeys is not None:
+                for k in extrakeys.keys():
+                    if k not in self.index:
+                        self.index[k] = {}
+                    v = extrakeys[k]
+                    if v not in self.index[k]:
+                        self.index[k][v] = [pk]
+                    else:
+                        self.index[k][v].append(pk)
+        except IndexError as e:
+            print(f"Cache {self.name} add error {e} pk:{pk} extrakeys: {extrakeys} value {value}")
+
+    def get(self, pk):
+        return self.data[pk]
+
+    def get_with_default(self, pk, default=None):
+        if pk not in self.data:
+            return default
+        return self.data[pk]
+
+    def list_by_key(self, key, value):
+        if key not in self.index:
+            return None
+        if value not in self.index[key]:
+            return None
+        keylist = self.index[key][value]
+        valuelist = []
+        for pk in keylist:
+            valuelist.append(self.data[pk])
+        return valuelist
---
+++ storage.py
@@ -0,0 +1,49 @@
+"""
+Handles overall in-memory data.
+
+"""
+import os
+from cache import Cache
+from config import Config
+import json
+from model import Resource, BuildStep
+
+
+class Storage:
+    """Stores data in memory and handles loading and saving.
+
+    """
+
+    def __init__(self, mfgdocsapp):
+        self.cache_actions = Cache('actions')
+        self.cache_roles = Cache('roles')
+        self.cache_tools = Cache('tools')
+        self.cache_buildsteps = Cache('buildsteps')
+        self.cache_parts = Cache('parts')
+        self.cache_locations = Cache('locations')
+        self.cache_machines = Cache('machines')
+        self.cache_consumables = Cache('consumables')
+        self.mfgdocsapp = mfgdocsapp
+        self.load_resources()
+
+    def load_resources(self):
+        self.load_json(self.cache_roles, '/data/roles.json', Resource)
+        self.load_json(self.cache_tools, '/data/tools.json', Resource)
+        self.load_json(self.cache_actions, '/data/actions.json', Resource)
+        self.load_json(self.cache_parts, '/data/parts.json', Resource)
+        self.load_json(self.cache_locations, '/data/locations.json', Resource)
+        self.load_json(self.cache_machines, '/data/machines.json', Resource)
+        self.load_json(self.cache_consumables, '/data/consumables.json', Resource)
+        self.load_json(self.cache_buildsteps, '/data/buildsteps.json', BuildStep)
+
+    def load_json(self, cache, filename, classname):
+        f_name = Config.workdir+filename.replace('/', os.sep)
+        try:
+            with open(f_name, mode='rt', encoding='utf-8') as json_file:
+                json_data = json.load(json_file)
+                for key in json_data.keys():
+                    item = classname()
+                    item.from_dict(json_data[key])
+                    cache.add(key, item, item.extrakeys())
+        except OSError as e:
+            self.mfgdocsapp.log(f'storage.load_json error: {e}')
