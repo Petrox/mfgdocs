888c6551f8a5db487c3b6c9b88fb4f7e84de4fd2
Peter Kopias <peter.kopias@rainboat.io>
2023-11-18 04:25:27 +0100

    add stylized items to the ingredidentlisteditor

--- ingredidentlisteditor.py
+++ ingredidentlisteditor.py
@@ -2,19 +2,31 @@
 A user control to edit a list of resources and their quantities.
 """
 import flet as ft
+from frontend import Frontend
+from storage import Storage
 
 
 class IngredientListEditor(ft.UserControl):
     """Item list and quantity editor."""
-    def __init__(self, label, items=None, color=ft.colors.ON_PRIMARY):
+
+    def __init__(self, label, items=None, color=ft.colors.ON_PRIMARY, resource_type=None, storage:Storage=None):
         super().__init__()
+        self.storage = storage
         if items is None:
             items = {}
         self.color = color
         self.label = label
         self.items = items
         self.controls = []
-        self.item_type = None
+        self.resource_type = resource_type
+
+    def get_searchitem(self, obj) -> ft.Control:
+        if Frontend.singleton_frontend is None:
+            return ft.Text('Frontend.singleton_frontend is None')
+        if obj is None:
+            return ft.Text('get_searchitem(None)')
+        print(f'get_searchitem({obj})')
+        return Frontend.singleton_frontend.get_searchresultitem(obj)
 
     def click_remove(self, event):
         key = event.control.data['key']
@@ -31,18 +43,18 @@ class IngredientListEditor(ft.UserControl):
         if found is not None:
             self.maincontrol.controls.pop(found)
 
-
-
     def set_amount(self, event):
         key = event.control.data['key']
         self.items[key] = event.control.value
         self.maincontrol.update()
 
-
-
     def build(self):
         self.controls = [ft.Text(self.label, color=self.color, style=ft.TextThemeStyle.LABEL_LARGE)]
         for k, v in self.items.items():
+            item = self.storage.get_resource_by_type_and_key(res_type=self.resource_type, key=k)
+            searchitem = self.get_searchitem(item)
+            if searchitem is None:
+                searchitem = ft.Text(f'Unknown: {k}', style=ft.TextThemeStyle.LABEL_LARGE, selectable=True)
             item_ctrl = ft.Row(data={'key': k, 'value': v},
                                controls=[ft.TextField(label='Amount',
                                                       value=v,
@@ -52,7 +64,8 @@ class IngredientListEditor(ft.UserControl):
                                                       data={'key': k, 'value': v},
                                                       on_change=self.set_amount),
                                          ft.Text(' x ', style=ft.TextThemeStyle.LABEL_SMALL),
-                                         ft.Text(k, style=ft.TextThemeStyle.LABEL_LARGE,selectable=True),
+                                         searchitem,
+                                         # ft.Text(k, style=ft.TextThemeStyle.LABEL_LARGE,selectable=True),
                                          ft.IconButton(icon=ft.icons.DELETE, data={'key': k, 'value': v},
                                                        on_click=self.click_remove)])
             self.controls.append(item_ctrl)
